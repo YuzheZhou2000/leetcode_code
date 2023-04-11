9524d3928bf4bd218249088f9ce3609f18281416
zhouyuzhe <zhou.yuzhe2000@gmail.com>
2023-04-11 11:05:01 +0800

    0411

--- 494.cpp
+++ 494.cpp
@@ -1,32 +1,39 @@
-# include <bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
     int res = 0;
     int len = 0;
-    int findTargetSumWays(vector<int>& nums, int target) {
+    int findTargetSumWays(vector<int> &nums, int target)
+    {
         len = nums.size();
-        findnum(nums, target, 0,0);
+        findnum(nums, target, 0, 0);
         return res;
-
     }
 
-    void findnum(vector<int>& nums, int target, int start,int sum){
-        if(start < len){
-            if(sum == target){
+    void findnum(vector<int> &nums, int target, int start, int sum)
+    {
+        if (start == len)
+        {
+            if (sum == target)
+            {
                 res++;
+                return;
             }
-            findnum(nums,target, start+1,sum + nums[start]);
-            findnum(nums,target, start+1,sum - nums[start]);
-        }       
+        }
+        if (start < len)
+        {
+            findnum(nums, target, start + 1, sum + nums[start]);
 
-        return;
+            findnum(nums, target, start + 1, sum - nums[start]);
+        }
     }
 };
-
-int main(){
+int main()
+{
 
     return 0;
 }
