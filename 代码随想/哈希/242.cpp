a3cf0c477a36114ae5023c2cc3d12cc558d2c486
zhouyuzhe <zhou.yuzhe2000@gmail.com>
2023-11-21 14:08:47 +0800

    哈希

---
+++ 代码随想/哈希/242.cpp
@@ -0,0 +1,19 @@
+# include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    /*
+    本题目是考察是不是两个异位字符串  
+    这样的话  只需要根据第一个字符串创建一个哈希表
+    然后根据哈希表进行检索即可
+    */
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map
+
+    }
+};
+
+int main(){
+    return 0;
+}
