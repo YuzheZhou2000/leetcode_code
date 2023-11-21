8cad2fe53f90139e2fcd231fb4434b006832779b
zhouyuzhe <zhou.yuzhe2000@gmail.com>
2023-11-21 15:39:27 +0800

    哈希

--- 代码随想/哈希/242.cpp
+++ 代码随想/哈希/242.cpp
@@ -1,19 +1,41 @@
-# include<bits/stdc++.h>
+# include <bits/stdc++.h>
 using namespace std;
 
+/**
+ * 因为涉及到的两个字符串的异位数字匹配问题
+ * 并且题目中给出来了是一个仅仅包含小写字母的问题
+ * 因此其实可以使用一个长度为26的数组来实现
+ * 其中数组的每一个位置上的值就代表了对应的元素出现的次数
+ * 在第二遍遍历的时候，只要保证每一个数组的元素都是0就可以了
+*/
 class Solution {
-    /*
-    本题目是考察是不是两个异位字符串  
-    这样的话  只需要根据第一个字符串创建一个哈希表
-    然后根据哈希表进行检索即可
-    */
 public:
     bool isAnagram(string s, string t) {
-        unordered_map
+        // 首先申请一个长度为26的数组
+        vector<int> temp_nums(26,0);
+        for (const auto & char_char : s){
+            temp_nums[char_char - 'a'] ++;            
+        }
+        
+        for (const auto & char_char : s){
+            temp_nums[char_char - 'a'] --;            
+        }
 
+        for (const auto & i : temp_nums){
+            if (i != 0){
+                return false;
+            }
+        }
+        return true;
     }
 };
 
 int main(){
+    vector<int> temp_nums(26,0);
+    string S = "aaaab";
+    for (const auto & char_char : S){
+        cout <<char_char-'a'<<endl;
+    }
+
     return 0;
 }
