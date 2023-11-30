93ad2d3cf911eeb36df73b5d801d904e6f1b46d1
zhouyuzhe <zhou.yuzhe2000@gmail.com>
2023-11-30 19:25:35 +0800

    搜索二叉树变双向链表

--- face2offer/offerOnbook/25.cpp
+++ face2offer/offerOnbook/25.cpp
@@ -16,7 +16,7 @@ struct TreeNode
 class Solution
 {
 public:
-    TreeNode *perNode = nullptr;
+    TreeNode *perNode; // 代表的是前一个节点，因为要变为双向链表
     TreeNode *Convert(TreeNode *pRootOfTree)
     {
         // 如果最基础的解法，就是通过中序遍历得到一个排序序列，然后构建双向链表
@@ -25,9 +25,32 @@ public:
         {
             return pRootOfTree;
         }
+        TreeNode *ans = pRootOfTree;
+        // 双向链表的开头的第一个节点就二叉搜索树最左侧的节点
+        while (ans->left)
+        {
+            ans = ans->left;
+        }
+        inorder(pRootOfTree);
+        return ans;
+    }
+    void inorder(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            // 如果是空的  直接返回
+            return;
+        }
+        inorder(root->left);
+        root->left = perNode;
+        if (perNode != nullptr)
+        {
+            perNode->right = root;
+        }
+        perNode = root;
+        inorder(root->right);
     }
 };
-
 int main()
 {
     return 0;
