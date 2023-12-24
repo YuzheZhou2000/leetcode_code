ceb1773b896ab59cb60d9f6c8830e5b4bfab33b1
zhouyuzhe <zhou.yuzhe2000@gmail.com>
2023-12-24 18:56:57 +0800

    最大值

--- face2work/maxNumN.cpp
+++ face2work/maxNumN.cpp
@@ -3,69 +3,99 @@ using namespace std;
 
 /**
  * 小于n的最大的数
-*/
+ */
 
-bool dfs(vector<int> & nums, vector<int> & target, vector<int>& ans, int index){
-    if (index >= target.size())return true;
-    int val = target[index];// 当前数位上许需要比较的元素
-    int i = nums.size()-1;
-    for (;i>=0;i--){
-        if (nums[i] == val){
-            // 这个时候这一个位置上的数字一样了，需要比较下面的元素 了就
-            ans.emplace_back(nums[i]);
-            dfs(nums, target, ans, index+1);
-            break;
-        }
-        if(nums[i] < val){
-            // 这个位已经笑了  以后全是最大的
-            ans.emplace_back(nums[i]);
-            int  n = target.size()- index-1;
-            while(n--){
-                ans.emplace_back(nums[nums.size()-1]);
+bool dfs(vector<int> &vec, vector<int> &nums, vector<int> &vecAns, int index, bool flag)
+{
+    /**
+     * vec: 候选的字符集  单调递减
+     * nums: 切割以后每一个数位上的值
+     * vecAns: 存放的结果值
+     * index：目标索引
+     * flag : 是不是可以直接往后面压入候选集合最大了
+     */
+    // 匹配当前index位置上的元素的值
+
+    if (index == nums.size())
+        return true;
+    if (flag)
+    {
+        // 直接压入最大的 然后index游标后移，一直循环到最后就可以了
+        vecAns.push_back(vec[vec.size() - 1]);
+        return dfs(vec, nums, vecAns, index + 1, true);
+    }
+    else
+    {
+        // 如果不是，那么就需要一个比较
+        // 比较什么呢。比较是正好相等，还是之前有过比他大的了
+        int tem = nums[index];
+        for (int i = 0; i < vec.size(); i++)
+        {
+            // 这个循环的目的就是为了看
+            if (tem == vec[i])
+            {
+                // 当前的值和原来的值是一样的
+                vecAns.push_back(vec[i]);
+                if (dfs(vec, nums, vecAns, index + 1, false))
+                    return true;
+                else
+                    // 如果返回了false 那么就说后面的存在都打了
+                    vecAns.pop_back();
             }
-            return;
+            else if (tem > vec[i])
+            {
+                // 以后全部是最小的元素
+                vecAns.push_back(vec[i]);
+                if (dfs(vec, nums, vecAns, index + 1, true))
+                    return true;
+                else
+                    vecAns.pop_back();
+            }
         }
-        
+        // 数组中最小的元素都大于数字该位的值
+        if (index != 0)
+            return false;
+        else
+            // 少了一位,直接放最大的
+            return dfs(vec, nums, vecAns, index + 1, true);
     }
-    // 这个使用全是大于当权元素得了
-    if(i < 0){
-        // 备选的所有值都大于当前元素
-        if (index == 0){
-            ans.push_back(0);
-            dfs(nums, target, ans, index+1);
-        }else{
-            // 这时候应该让上面的位置的数字比原来小了
-        }
-    }
-    return;
 }
 
-int maxNum(vector<int> vec, int target){
+int maxNum(vector<int> vec, int target)
+{
     int ans = 0;
+    if (vec.size() == 0 || target <= 0)
+        return ans;
+
     // 首先做一个分割，把target中的每一位都分割出来
-    sort(vec.begin(), vec.end());
     vector<int> nums;
-    while(target){
-        int tem = target%10;
+    while (target)
+    {
+        int tem = target % 10;
         nums.emplace_back(tem);
-        target = target/10;
+        target = target / 10;
     }
     reverse(nums.begin(), nums.end());
 
+    sort(vec.begin(), vec.end());
+    reverse(vec.begin(), vec.end());
+
     // 下面开始使用递归的方法解决本问题
     vector<int> vecAns;
-    dfs(vec, nums, vecAns, 0);
+    dfs(vec, nums, vecAns, 0, false);
 
-    for (auto &i : nums){
-        cout <<i<<"  ";
+    for (int i = 0; i < vecAns.size(); i++)
+    {
+        ans = ans * 10 + vecAns[i];
     }
-
     return ans;
 }
 
-int main(){
-    vector<int> vec = {1,2,4,6,8,9};
-    int target = 873;
-    maxNum(vec, target);
+int main()
+{
+    vector<int> nums = {1, 2, 4, 6, 8, 9};
+    int target = 803;
+    int ans = maxNum(nums, target);
+    cout << "ans: " << ans << endl;
     return 0;
 }
